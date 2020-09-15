33cb5b4155d50bf8092d6593137312e7a1845652
BuildTools <unconfigured@null.spigotmc.org>
2020-09-15 20:12:24 +0200

    added right-click event listener

--- src/main/java/com/finaldaylight/snowsumo/Main.java
+++ src/main/java/com/finaldaylight/snowsumo/Main.java
@@ -8,8 +8,10 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Snowball;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -17,6 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public final class Main extends JavaPlugin implements Listener {
 
@@ -71,6 +74,13 @@ public final class Main extends JavaPlugin implements Listener {
     }
     @EventHandler
     public void onClick(PlayerInteractEvent event){
-        
+        if(event.getPlayer().getInventory().getItemInMainHand().getType().equals(Material.BLAZE_ROD))
+            if (Objects.requireNonNull(event.getPlayer().getInventory().getItemInMainHand().getItemMeta()).hasLore()){
+                //right click event
+                Player player = (Player) event.getPlayer();
+                if (event.getAction()== Action.RIGHT_CLICK_AIR) {
+                    player.launchProjectile(Snowball.class);
+                }
+            }
     }
 }
